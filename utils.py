787567ab8abf0d139ab720f1e9a427762337655e
Dadakhalander <46879813+dadakhalander@users.noreply.github.com>
2025-05-13 21:26:47 +0200

    utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.models import save_model
+
+def save_models(relu_model, tanh_model):
+    """Save trained models to disk"""
+    relu_model.save('models/relu_model.h5')
+    tanh_model.save('models/tanh_model.h5')
+    print("Models saved successfully!")
